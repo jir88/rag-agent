f0e5131434d6da782498bb1a36225356e5ec8c8a
jir88 <6749336+jir88@users.noreply.github.com>
2025-09-25 09:58:54 -0500

    Simple testing script using Granite 4 Tiny model.

---
+++ test_script.py
@@ -0,0 +1,24 @@
+from agent import RagAgent
+
+if __name__ == "__main__":
+    # create the agent itself
+    # model="huggingface/Qwen/Qwen2.5-Coder-32B-Instruct"
+    # api_key=None
+    # base_url=None
+    # model="openai/gemma-3n-E4B-it-UD-Q5_K_XL-cpu"
+    model="openai/ibm-granite_granite-4.0-tiny-preview-Q5_K_S-cpu"
+    # model="openai/gemma-3-4B-it-UD-Q4_K_XL-cpu"
+    api_key="sk-fake"
+    base_url="http://127.0.0.1:8080/v1"
+    agent = RagAgent(
+        llm=model, 
+        tools={
+            # "web_search": DuckDuckGoSearchTool(max_results=10),
+            # "visit_webpage": VisitWebpageTool(),
+            # "wikipedia_search": WikipediaSearchTool(max_results=4)
+        },
+        api_key=api_key,
+        base_url=base_url
+    )
+
+    agent.submit_question("Search online for recent research on bile acid metabolism and write a detailed report describing any significant findings.")
