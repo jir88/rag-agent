eea4e2d9d4d84be1f358e38f477f6cfd9cea458d
jir88 <6749336+jir88@users.noreply.github.com>
2025-10-03 18:06:32 -0500

    Update test model to Granite 4.0 Tiny release version.

--- test_script.py
+++ test_script.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # api_key=None
     # base_url=None
     # model="openai/gemma-3n-E4B-it-UD-Q5_K_XL-cpu"
-    model="openai/ibm-granite_granite-4.0-tiny-preview-Q5_K_S-cpu"
+    model="openai/granite-4.0-h-tiny-UD-Q5_K_XL-cpu"
     # model="openai/gemma-3-4B-it-UD-Q4_K_XL-cpu"
     api_key="sk-fake"
     base_url="http://127.0.0.1:8080/v1"
