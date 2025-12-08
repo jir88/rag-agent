317436e0113c0beba857d7becfdcc72ce7d77add
jir88 <6749336+jir88@users.noreply.github.com>
2025-12-08 10:46:56 -0600

    Start work on specialized agent to monitor newly-published literature and evaluate its relevance.

---
+++ new_lit_monitor.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == "__main__":
+    # create the agent itself
+    # model="huggingface/Qwen/Qwen2.5-Coder-32B-Instruct"
+    # api_key=None
+    # base_url=None
+    # model="openai/gemma-3n-E4B-it-UD-Q5_K_XL-cpu"
+    model="openai/granite-4.0-h-tiny-UD-Q5_K_XL-cpu"
+    # model="openai/gemma-3-4B-it-UD-Q4_K_XL-cpu"
+    api_key="sk-fake"
+    base_url="http://127.0.0.1:8080/v1"
