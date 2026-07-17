2f7ba048cd400f1a77f45b44840bfe135269171e
jir88 <6749336+jir88@users.noreply.github.com>
2026-07-16 21:35:36 -0500

    Add reference results tab to the A/B eval GUI.

--- gui_ab_eval_comparison.py
+++ gui_ab_eval_comparison.py
@@ -1,9 +1,37 @@
-from nicegui import app, binding, ui, events, elements
+from nicegui import ui, events, elements
+
+import sys
+
+from monitor import Article,LitMonitorState
 
 class ABEvalGUI:
     """
     A GUI for side-by-side comparison of evaluations run under two different conditions.
     """
+
+    # ===== Reference Results =============
+    ref_agent_results: LitMonitorState
+    ref_current_article: Article
+    """Currently selected article, if any."""
+
+    # GUI elements
+    ref_ta_system_prompt: elements.textarea.Textarea
+    """Text area showing the system prompt used by the agent."""
+    ref_ta_relevance_prompt: elements.textarea.Textarea
+    """Text area showing the format used to present articles for evaluation."""
+
+    ref_table_results_data: elements.table.Table
+    """Table showing articles in the current result."""
+    ref_label_title: elements.label.Label
+    """Label to hold selected article title."""
+    ref_label_abstract: elements.label.Label
+    """Label to hold selected article abstract."""
+    ref_label_query: elements.label.Label
+    """Label to hold the criteria for whether an article is relevant."""
+    ref_cb_article_relevant: elements.checkbox.Checkbox
+    """Check box showing/controlling whether article is judged as relevant."""
+    ref_ta_article_eval: elements.textarea.Textarea
+    """Text area showing the article relevance evaluation."""
 
     def __init__(self):
         """Initialize the GUI."""
@@ -15,32 +43,190 @@ class ABEvalGUI:
     def setup_ui(self):
         """Build the GUI itself."""
         # define navigation tabs
-        with ui.tabs().classes('w-full') as tabs:
-            tab_a = ui.tab('Condition A')
-            self.tab_b = ui.tab('Condition B')
-            self.tab_comparison = ui.tab('Comparison')
-            self.tab_settings = ui.tab("Settings")
+        with ui.header().classes('bg-dark'):
+            with ui.tabs().classes('w-full') as tabs:
+                tab_ref = ui.tab('Reference')
+                self.tab_memory = ui.tab('Condition A')
+                self.tab_archive = ui.tab('Condition B')
+                self.tab_settings = ui.tab("Comparison")
         # define contents of each tab
-        with ui.tab_panels(tabs, value=tab_a).classes('w-7/8'):
-            # ---------- CONDITION A TAB ----------------
-            self.panel_a = ui.tab_panel(tab_a)
-            with self.panel_a:
-                # file uploader to select the evaluation results we want to look at
-                eval_result_uploader = ui.upload(
-                    on_upload=self.handle_upload,
-                    max_file_size=10e6,
-                    multiple=False,
-                    # max_files=1,
-                    auto_upload=True,
-                    label="Upload condition A results:"
+        with ui.tab_panels(tabs, value=tab_ref).classes('w-7/8'):
+
+            # ------ MAIN TAB -------------
+
+            ref_panel = ui.tab_panel(tab_ref)
+            with ref_panel:
+                with ui.row():
+                    # file uploader to select the evaluation results we want to look at
+                    ref_eval_result_uploader = ui.upload(
+                        on_upload=self.handle_ref_upload,
+                        max_file_size=10e6,
+                        multiple=False,
+                        max_files=1,
+                        auto_upload=True,
+                        label="Upload evaluation results:"
+                    )
+                    ref_eval_result_uploader.props('accept=.json')
+
+                    ui.button(text="Save", icon='save', on_click=self.handle_ref_save)
+                
+                # common settings for all articles
+
+                ui.label("System prompt:").classes("text-2xl")
+                self.ref_ta_system_prompt = ui.textarea(
+                    placeholder="Agent's system prompt.",
+                    on_change=self.handle_ref_prompt_update
+                ).classes("text-base w-7/8")
+
+                ui.label("Article relevance prompt:").classes("text-2xl")
+                self.ref_ta_relevance_prompt = ui.textarea(
+                    placeholder="Prompt used to present articles for evaluation.",
+                    on_change=self.handle_ref_prompt_update
+                ).classes("text-base w-7/8")
+
+                # table showing the articles in this evaluation run
+                columns = [
+                    {'name': 'title', 'label': 'Title', 'field': 'title', 'required': True, 'align': 'left'},
+                    {'name': 'date', 'label': 'Published', 'field': 'date', 'sortable': True},
+                    {'name': 'is_relevant', 'label': 'Relevant?', 'field':'is_relevant', 'sortable': True}
+                ]
+                self.ref_table_results_data = ui.table(
+                    rows=[], 
+                    columns=columns,
+                    selection='single', 
+                    row_key='pubmed_id',
+                    pagination=3,
+                    on_select=self.handle_ref_result_selection,
+                ).classes("w-7/8")
+
+                # information about an individual article
+
+                self.ref_label_title = ui.label("Title").classes("text-3xl w-7/8")
+                self.ref_label_abstract = ui.label().classes("text-base w-7/8")
+                # the criteria for article relevance
+                ui.label("Relevance criteria:").classes("text-2xl")
+                self.ref_label_query = ui.label().classes("text-base w-7/8")
+                # what the LLM thought about the article
+
+                self.ref_cb_article_relevant = ui.checkbox(
+                    text="Article relevant to query",
+                    value=False,
+                    on_change=self.handle_ref_result_update
                 )
-                eval_result_uploader.props('accept=.csv')
+                ui.label("Why is/isn't the article relevant?").classes("text-2xl")
+                self.ref_ta_article_eval = ui.textarea(
+                    placeholder="Write explanation here.",
+                    on_change=self.handle_ref_result_update
+                ).classes("text-base w-7/8")
             
             # --------------- SETTINGS TAB -------------------
             
             with ui.tab_panel(self.tab_settings):
                 # toggle to control light/dark theme
                 ui.switch('Dark mode').bind_value(self.dark_setting)
+    
+    async def handle_ref_upload(self, e: events.UploadEventArguments):
+        """
+        Uploads an agent result file and loads the data into the reference results table.
+
+        Args:
+            e: The file upload event.
+        """
+        # Read the result file
+        try:
+            self.ref_agent_results = LitMonitorState.model_validate_json(await e.file.text())
+        except Exception as e:
+            ui.notify(
+                message=f"Error reading evaluation file: {e}",
+                type='warning',
+                multi_line=True
+            )
+            sys.exit(1)
+        
+        # clear the upload widget
+        e.sender.reset()
+
+        # load prompts
+        self.ref_ta_system_prompt.value = self.ref_agent_results.agent_system_prompt
+        self.ref_ta_relevance_prompt.value = self.ref_agent_results.article_relevance_prompt
+        
+        # populate the table
+        result_rows = []
+        index = 0
+        for article in self.ref_agent_results.new_articles:
+            row_data = {
+                "index": index,
+                "pubmed_id": article.pubmed_id,
+                "date": article.date,
+                "title": article.title,
+                "source": article.source,
+                "is_relevant": article.is_relevant,
+                "abstract": article.abstract,
+                "query": self.ref_agent_results.topic_description,
+                "evaluation": article.evaluation
+            }
+            result_rows.append(row_data)
+            index += 1
+        self.ref_table_results_data.rows = result_rows
+
+    def handle_ref_save(self):
+        """Save the monitor results to a JSON file."""
+        if self.agent_results is None:
+            return
+        output_file_txt = self.ref_agent_results.model_dump_json(indent=2)
+        # show save dialog
+        ui.download.content(
+            content=output_file_txt,
+            filename="monitor_results.json",
+            media_type="application/json"
+        )
+
+    def handle_ref_result_selection(self, e: events.TableSelectionEventArguments):
+        # if selection is empty, clear the data
+        if len(e.selection) == 0:
+            self.ref_current_article = None
+            self.ref_label_title.set_text("Title")
+            self.ref_label_abstract.set_text("Abstract")
+            self.ref_label_query.set_text("Query")
+            self.ref_cb_article_relevant.set_value(False)
+            self.ref_ta_article_eval.set_value("")
+            return
+        row_data = e.selection[0]
+        # set current article
+        self.ref_current_article = self.ref_agent_results.new_articles[row_data['index']]
+
+        self.ref_label_title.set_text(row_data['title'])
+        self.ref_label_abstract.set_text(row_data['abstract'])
+        self.ref_label_query.set_text(row_data['query'])
+        self.ref_cb_article_relevant.set_value(row_data['is_relevant'])
+        self.ref_ta_article_eval.set_value(row_data['evaluation'])
+    
+    def handle_ref_result_update(self):
+        """Called when result relevance or evaluation is updated."""
+        # if nothing selected, skip
+        if self.ref_current_article is None:
+            return
+        self.ref_current_article.is_relevant = self.ref_cb_article_relevant.value
+        self.ref_current_article.evaluation = self.ref_ta_article_eval.value
+        # search for article pmid in table to get index
+        row = None
+        for r in self.ref_table_results_data.rows:
+            if r['pubmed_id'] == self.ref_current_article.pubmed_id:
+                row = r
+                break
+        if row is None:
+            print("Warning! Article not found!")
+            return
+        # pull row
+        row['is_relevant'] = self.ref_current_article.is_relevant
+        row['evaluation'] = self.ref_current_article.evaluation
+        # change the data
+        self.ref_table_results_data.update()
+    
+    def handle_ref_prompt_update(self):
+        """Called when one of the agent prompts is updated."""
+        self.ref_agent_results.agent_system_prompt = self.ref_ta_system_prompt.value
+        self.ref_agent_results.article_relevance_prompt = self.ref_ta_relevance_prompt.value
     
     async def handle_upload(e: events.UploadEventArguments):
         """
