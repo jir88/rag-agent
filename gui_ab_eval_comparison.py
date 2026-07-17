01d50f7895a0a3d1e57abd5e9c9a5e2ac46fd4fa
jir88 <6749336+jir88@users.noreply.github.com>
2026-07-17 17:41:31 -0500

    Add condition B tab to the A/B eval GUI.

--- gui_ab_eval_comparison.py
+++ gui_ab_eval_comparison.py
@@ -57,6 +57,30 @@ class ABEvalGUI:
     condA_ta_article_eval: elements.textarea.Textarea
     """Text area showing the article relevance evaluation."""
 
+    # ===== Condition B Results =============
+    condB_agent_results: LitMonitorState
+    condB_current_article: Article
+    """Currently selected article, if any."""
+
+    # GUI elements
+    condB_ta_system_prompt: elements.textarea.Textarea
+    """Text area showing the system prompt used by the agent."""
+    condB_ta_relevance_prompt: elements.textarea.Textarea
+    """Text area showing the format used to present articles for evaluation."""
+
+    condB_table_results_data: elements.table.Table
+    """Table showing articles in the current result."""
+    condB_label_title: elements.label.Label
+    """Label to hold selected article title."""
+    condB_label_abstract: elements.label.Label
+    """Label to hold selected article abstract."""
+    condB_label_query: elements.label.Label
+    """Label to hold the criteria for whether an article is relevant."""
+    condB_cb_article_relevant: elements.checkbox.Checkbox
+    """Check box showing/controlling whether article is judged as relevant."""
+    condB_ta_article_eval: elements.textarea.Textarea
+    """Text area showing the article relevance evaluation."""
+
     def __init__(self):
         """Initialize the GUI."""
         # set up state
@@ -71,8 +95,8 @@ class ABEvalGUI:
             with ui.tabs().classes('w-full') as tabs:
                 tab_ref = ui.tab('Reference')
                 tab_cond_a = ui.tab('Condition A')
-                self.tab_archive = ui.tab('Condition B')
-                self.tab_settings = ui.tab("Comparison")
+                tab_cond_b = ui.tab('Condition B')
+                tab_comparison = ui.tab("Comparison")
         # define contents of each tab
         with ui.tab_panels(tabs, value=tab_ref).classes('w-7/8'):
 
@@ -208,6 +232,73 @@ class ABEvalGUI:
                 self.condA_ta_article_eval = ui.textarea(
                     placeholder="Write explanation here.",
                     on_change=self.handle_condA_result_update
+                ).classes("text-base w-7/8")
+
+            # ------ CONDITION B TAB -------------
+
+            condB_panel = ui.tab_panel(tab_cond_b)
+            with condB_panel:
+                with ui.row():
+                    # file uploader to select the evaluation results we want to look at
+                    condB_eval_result_uploader = ui.upload(
+                        on_upload=self.handle_condB_upload,
+                        max_file_size=10e6,
+                        multiple=False,
+                        max_files=1,
+                        auto_upload=True,
+                        label="Upload evaluation results:"
+                    )
+                    condB_eval_result_uploader.props('accept=.json')
+
+                    ui.button(text="Save", icon='save', on_click=self.handle_condB_save)
+                
+                # common settings for all articles
+
+                ui.label("System prompt:").classes("text-2xl")
+                self.condB_ta_system_prompt = ui.textarea(
+                    placeholder="Agent's system prompt.",
+                    on_change=self.handle_condB_prompt_update
+                ).classes("text-base w-7/8")
+
+                ui.label("Article relevance prompt:").classes("text-2xl")
+                self.condB_ta_relevance_prompt = ui.textarea(
+                    placeholder="Prompt used to present articles for evaluation.",
+                    on_change=self.handle_condB_prompt_update
+                ).classes("text-base w-7/8")
+
+                # table showing the articles in this evaluation run
+                columns = [
+                    {'name': 'title', 'label': 'Title', 'field': 'title', 'required': True, 'align': 'left'},
+                    {'name': 'date', 'label': 'Published', 'field': 'date', 'sortable': True},
+                    {'name': 'is_relevant', 'label': 'Relevant?', 'field':'is_relevant', 'sortable': True}
+                ]
+                self.condB_table_results_data = ui.table(
+                    rows=[], 
+                    columns=columns,
+                    selection='single', 
+                    row_key='pubmed_id',
+                    pagination=3,
+                    on_select=self.handle_condB_result_selection,
+                ).classes("w-7/8")
+
+                # information about an individual article
+
+                self.condB_label_title = ui.label("Title").classes("text-3xl w-7/8")
+                self.condB_label_abstract = ui.label().classes("text-base w-7/8")
+                # the criteria for article relevance
+                ui.label("Relevance criteria:").classes("text-2xl")
+                self.condB_label_query = ui.label().classes("text-base w-7/8")
+                # what the LLM thought about the article
+
+                self.condB_cb_article_relevant = ui.checkbox(
+                    text="Article relevant to query",
+                    value=False,
+                    on_change=self.handle_condB_result_update
+                )
+                ui.label("Why is/isn't the article relevant?").classes("text-2xl")
+                self.condB_ta_article_eval = ui.textarea(
+                    placeholder="Write explanation here.",
+                    on_change=self.handle_condB_result_update
                 ).classes("text-base w-7/8")
     
     # ============ REFERENCE TAB EVENT HANDLERS ================
@@ -420,6 +511,111 @@ class ABEvalGUI:
         self.condA_agent_results.agent_system_prompt = self.condA_ta_system_prompt.value
         self.condA_agent_results.article_relevance_prompt = self.condA_ta_relevance_prompt.value
     
+    # ============ CONDITION B TAB EVENT HANDLERS ================
+
+    async def handle_condB_upload(self, e: events.UploadEventArguments):
+        """
+        Uploads an agent result file and loads the data into the reference results table.
+
+        Args:
+            e: The file upload event.
+        """
+        # Read the result file
+        try:
+            self.condB_agent_results = LitMonitorState.model_validate_json(await e.file.text())
+        except Exception as e:
+            ui.notify(
+                message=f"Error reading evaluation file: {e}",
+                type='warning',
+                multi_line=True
+            )
+            sys.exit(1)
+        
+        # clear the upload widget
+        e.sender.reset()
+
+        # load prompts
+        self.condB_ta_system_prompt.value = self.condB_agent_results.agent_system_prompt
+        self.condB_ta_relevance_prompt.value = self.condB_agent_results.article_relevance_prompt
+        
+        # populate the table
+        result_rows = []
+        index = 0
+        for article in self.condB_agent_results.new_articles:
+            row_data = {
+                "index": index,
+                "pubmed_id": article.pubmed_id,
+                "date": article.date,
+                "title": article.title,
+                "source": article.source,
+                "is_relevant": article.is_relevant,
+                "abstract": article.abstract,
+                "query": self.condB_agent_results.topic_description,
+                "evaluation": article.evaluation
+            }
+            result_rows.append(row_data)
+            index += 1
+        self.condB_table_results_data.rows = result_rows
+
+    def handle_condB_save(self):
+        """Save the monitor results to a JSON file."""
+        if self.agent_results is None:
+            return
+        output_file_txt = self.condB_agent_results.model_dump_json(indent=2)
+        # show save dialog
+        ui.download.content(
+            content=output_file_txt,
+            filename="monitor_results.json",
+            media_type="application/json"
+        )
+
+    def handle_condB_result_selection(self, e: events.TableSelectionEventArguments):
+        # if selection is empty, clear the data
+        if len(e.selection) == 0:
+            self.condB_current_article = None
+            self.condB_label_title.set_text("Title")
+            self.condB_label_abstract.set_text("Abstract")
+            self.condB_label_query.set_text("Query")
+            self.condB_cb_article_relevant.set_value(False)
+            self.condB_ta_article_eval.set_value("")
+            return
+        row_data = e.selection[0]
+        # set current article
+        self.condB_current_article = self.condB_agent_results.new_articles[row_data['index']]
+
+        self.condB_label_title.set_text(row_data['title'])
+        self.condB_label_abstract.set_text(row_data['abstract'])
+        self.condB_label_query.set_text(row_data['query'])
+        self.condB_cb_article_relevant.set_value(row_data['is_relevant'])
+        self.condB_ta_article_eval.set_value(row_data['evaluation'])
+    
+    def handle_condB_result_update(self):
+        """Called when result relevance or evaluation is updated."""
+        # if nothing selected, skip
+        if self.condB_current_article is None:
+            return
+        self.condB_current_article.is_relevant = self.condB_cb_article_relevant.value
+        self.condB_current_article.evaluation = self.condB_ta_article_eval.value
+        # search for article pmid in table to get index
+        row = None
+        for r in self.condB_table_results_data.rows:
+            if r['pubmed_id'] == self.condB_current_article.pubmed_id:
+                row = r
+                break
+        if row is None:
+            print("Warning! Article not found!")
+            return
+        # pull row
+        row['is_relevant'] = self.condB_current_article.is_relevant
+        row['evaluation'] = self.condB_current_article.evaluation
+        # change the data
+        self.condB_table_results_data.update()
+    
+    def handle_condB_prompt_update(self):
+        """Called when one of the agent prompts is updated."""
+        self.condB_agent_results.agent_system_prompt = self.condB_ta_system_prompt.value
+        self.condB_agent_results.article_relevance_prompt = self.condB_ta_relevance_prompt.value
+    
     async def handle_upload(e: events.UploadEventArguments):
         """
         Uploads evaluation data file and adds the data to the table.
@@ -474,6 +670,60 @@ class ABEvalGUI:
         else:
             npv = 0.0
         label_npv.text = f"NPV: {npv:.1%}"
+
+
+    # calculate whole-dataset statistics
+    # y_true = eval_results_data['gold_standard'].to_numpy(dtype=np.bool)
+    # y_pred = eval_results_data['is_relevant'].to_numpy(dtype=np.bool)
+    # cm = confusion_matrix(
+    #     y_true=y_true, y_pred=y_pred,
+    # )
+    # table_conf_mat.rows = [
+    #     {'row_label': 'Irrelevant', 'negative': cm[0, 0], 'positive': cm[0, 1]},
+    #     {'row_label': 'Relevant', 'negative': cm[1, 0], 'positive': cm[1, 1]},
+    # ]
+
+    # # calculate accuracy
+    # pred_accuracy = np.sum(y_true == y_pred)/len(y_true)
+    # label_accuracy.text = f"Accuracy: {pred_accuracy:.1%}"
+    # # calculate PPV
+    # true_positives = np.sum((y_true == 1) & (y_pred == 1))
+    # false_positives = np.sum((y_true == 0) & (y_pred == 1))
+    # if (true_positives + false_positives) > 0:
+    #     ppv = true_positives/(true_positives + false_positives)
+    # else:
+    #     ppv = 0.0
+    # label_ppv.text = f"PPV: {ppv:.1%}"
+    # # calculate NPV
+    # true_negatives = np.sum((y_true == 0) & (y_pred == 0))
+    # false_negatives = np.sum((y_true == 1) & (y_pred == 0))
+    # if (true_negatives + false_negatives) > 0:
+    #     npv = true_negatives/(true_negatives + false_negatives)
+    # else:
+    #     npv = 0.0
+    # label_npv.text = f"NPV: {npv:.1%}"
+
+    
+# display summary statistics about the results
+# with ui.row():
+#     # confusion matrix
+#     columns = [
+#         {'name': 'row_label', 'label': 'True relevance', 'field': 'row_label'},
+#         {'name': 'negative', 'label': 'Pred. irrelevant', 'field': 'negative'},
+#         {'name': 'positive', 'label': 'Pred. relevant', 'field': 'positive'},
+#     ]
+#     # placeholder data
+#     rows = [
+#         {'row_label': 'Irrelevant', 'positive': 0, 'negative': 0},
+#         {'row_label': 'Relevant', 'positive': 0, 'negative': 0},
+#     ]
+#     table_conf_mat = ui.table(rows=rows, columns=columns, row_key='row_label')
+
+#     # miscellaneous stats
+#     with ui.column():
+#         label_accuracy = ui.label("Accuracy:")
+#         label_ppv = ui.label("PPV:")
+#         label_npv = ui.label("NPV:")
 
 gui = ABEvalGUI()
 ui.run(host='127.0.0.1', port=9092, title="New Lit A/B Eval")
